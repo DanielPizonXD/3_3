43a5079bf1a3636c61d72cab319ab0b384648968
DanielPizonXD <126687080+DanielPizonXD@users.noreply.github.com>
2023-03-29 08:04:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+
+    for (int a = 1; a < 11; a++) {
+      for (int b = 1; b < 11; b++) {
+        System.out.print(" " + a * b + " ");
+      }
+      System.out.println();
+    }
+  }
+}
